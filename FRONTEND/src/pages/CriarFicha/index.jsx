9155245a62532276812cc96151d4b55de3bd1599
IagoCF <iagofreits@gmail.com>
2025-05-26 19:42:51 -0300

    Mais campos da ficha + CSS derivados

--- FRONTEND/src/pages/CriarFicha/index.jsx
+++ FRONTEND/src/pages/CriarFicha/index.jsx
@@ -22,6 +22,13 @@ function CriarFicha() {
   const inputInteligencia = useRef()
   const inputSabedoria = useRef()
   const inputCarisma = useRef()
+  //Mods
+  const inputForcaMod = useRef()
+  const inputDestrezaMod = useRef()
+  const inputConstituicaoMod = useRef()
+  const inputInteligenciaMod = useRef()
+  const inputSabedoriaMod = useRef()
+  const inputCarismaMod = useRef()
 
   const inputCa = useRef()
   const inputIniciativa = useRef()
@@ -31,7 +38,58 @@ function CriarFicha() {
   const inputPontosVidaAtuais = useRef()
   const inputPontosVidaTemporarios = useRef()
 
-  const inputHistoria = useRef()
+  const inputInspiracao = useRef()
+  const inputProficiencia = useRef()
+  const inputSalvaguardaForca = useRef();
+  const inputNumeroSalvaguardaForca = useRef();
+  const inputSalvaguardaDestreza = useRef();
+  const inputNumeroSalvaguardaDestreza = useRef();
+  const inputSalvaguardaConstituicao = useRef();
+  const inputNumeroSalvaguardaConstituicao = useRef();
+  const inputSalvaguardaInteligencia = useRef();
+  const inputNumeroSalvaguardaInteligencia = useRef();
+  const inputSalvaguardaSabedoria = useRef();
+  const inputNumeroSalvaguardaSabedoria = useRef();
+  const inputSalvaguardaCarisma = useRef();
+  const inputNumeroSalvaguardaCarisma = useRef();
+
+  // Perícias
+  const inputPericiaAcrobacia = useRef();
+  const inputNumeroPericiaAcrobacia = useRef();
+  const inputPericiaArcanismo = useRef();
+  const inputNumeroPericiaArcanismo = useRef();
+  const inputPericiaAtletismo = useRef();
+  const inputNumeroPericiaAtletismo = useRef();
+  const inputPericiaAtuacao = useRef();
+  const inputNumeroPericiaAtuacao = useRef();
+  const inputPericiaEnganacao = useRef();
+  const inputNumeroPericiaEnganacao = useRef();
+  const inputPericiaFurtividade = useRef();
+  const inputNumeroPericiaFurtividade = useRef();
+  const inputPericiaHistoria = useRef();
+  const inputNumeroPericiaHistoria = useRef();
+  const inputPericiaIntimidacao = useRef();
+  const inputNumeroPericiaIntimidacao = useRef();
+  const inputPericiaIntuicao = useRef();
+  const inputNumeroPericiaIntuicao = useRef();
+  const inputPericiaInvestigacao = useRef();
+  const inputNumeroPericiaInvestigacao = useRef();
+  const inputPericiaLidarComAnimais = useRef();
+  const inputNumeroPericiaLidarComAnimais = useRef();
+  const inputPericiaMedicina = useRef();
+  const inputNumeroPericiaMedicina = useRef();
+  const inputPericiaNatureza = useRef();
+  const inputNumeroPericiaNatureza = useRef();
+  const inputPericiaPercepcao = useRef();
+  const inputNumeroPericiaPercepcao = useRef();
+  const inputPericiaPersuasao = useRef();
+  const inputNumeroPericiaPersuasao = useRef();
+  const inputPericiaPrestidigitacao = useRef();
+  const inputNumeroPericiaPrestidigitacao = useRef();
+  const inputPericiaReligiao = useRef();
+  const inputNumeroPericiaReligiao = useRef();
+  const inputPericiaSobrevivencia = useRef();
+  const inputNumeroPericiaSobrevivencia = useRef();
 
   function irParaHomeLogin() {
     navigate('/homelogin')
@@ -69,11 +127,17 @@ function CriarFicha() {
           <input className="campo xp" type="text" placeholder="XP" ref={inputXp} />
 
           {/* Atributos */}
+          <input className="campo forcamod" type="text" placeholder="FOR" ref={inputForcaMod} />
           <input className="campo forca" type="text" placeholder="FOR" ref={inputForca} />
+          <input className="campo destrezamod" type="text" placeholder="DES" ref={inputDestrezaMod} />
           <input className="campo destreza" type="text" placeholder="DES" ref={inputDestreza} />
+          <input className="campo constituicaomod" type="text" placeholder="CON" ref={inputConstituicaoMod} />
           <input className="campo constituicao" type="text" placeholder="CON" ref={inputConstituicao} />
+          <input className="campo inteligenciamod" type="text" placeholder="INT" ref={inputInteligenciaMod} />
           <input className="campo inteligencia" type="text" placeholder="INT" ref={inputInteligencia} />
+          <input className="campo sabedoriamod" type="text" placeholder="SAB" ref={inputSabedoriaMod} />
           <input className="campo sabedoria" type="text" placeholder="SAB" ref={inputSabedoria} />
+          <input className="campo carismamod" type="text" placeholder="CAR" ref={inputCarismaMod} />
           <input className="campo carisma" type="text" placeholder="CAR" ref={inputCarisma} />
 
           {/* Defesa e Vida */}
@@ -84,6 +148,116 @@ function CriarFicha() {
           <input className="campo pvMax" type="text" placeholder="PV Máximo" ref={inputPontosVida} />
           <input className="campo pvAtual" type="text" placeholder="PV Atual" ref={inputPontosVidaAtuais} />
           <input className="campo pvTemp" type="text" placeholder="PV Temporário" ref={inputPontosVidaTemporarios} />
+
+          {/* Inspiração */}
+          <input className="campo inspiracao" type="text" placeholder="Inspiração" ref={inputInspiracao} />
+
+          {/* Proficiência */}
+          <input className="campo proficiencia" type="text" placeholder="Bônus de Proficiência" ref={inputProficiencia} />
+
+          {/* Salvaguardas */}
+          <div className="campo salvaguardas">
+            <div className="linha-salvaguarda">
+              <input type="checkbox" className="bolinha bolinha-forca" ref={inputSalvaguardaForca} />
+              <input type="text" className="numero-salvaguarda numero-forca" maxLength={2} ref={inputNumeroSalvaguardaForca} />
+            </div>
+            <div className="linha-salvaguarda">
+              <input type="checkbox" className="bolinha bolinha-destreza" ref={inputSalvaguardaDestreza} />
+              <input type="text" className="numero-salvaguarda numero-destreza" maxLength={2} ref={inputNumeroSalvaguardaDestreza} />
+            </div>
+            <div className="linha-salvaguarda">
+              <input type="checkbox" className="bolinha bolinha-constituicao" ref={inputSalvaguardaConstituicao} />
+              <input type="text" className="numero-salvaguarda numero-constituicao" maxLength={2} ref={inputNumeroSalvaguardaConstituicao} />
+            </div>
+            <div className="linha-salvaguarda">
+              <input type="checkbox" className="bolinha bolinha-inteligencia" ref={inputSalvaguardaInteligencia} />
+              <input type="text" className="numero-salvaguarda numero-inteligencia" maxLength={2} ref={inputNumeroSalvaguardaInteligencia} />
+            </div>
+            <div className="linha-salvaguarda">
+              <input type="checkbox" className="bolinha bolinha-sabedoria" ref={inputSalvaguardaSabedoria} />
+              <input type="text" className="numero-salvaguarda numero-sabedoria" maxLength={2} ref={inputNumeroSalvaguardaSabedoria} />
+            </div>
+            <div className="linha-salvaguarda">
+              <input type="checkbox" className="bolinha bolinha-carisma" ref={inputSalvaguardaCarisma} />
+              <input type="text" className="numero-salvaguarda numero-carisma" maxLength={2} ref={inputNumeroSalvaguardaCarisma} />
+            </div>
+          </div>
+
+          {/* Perícias */}
+          <div className="campo pericias">
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-acrobacia" ref={inputPericiaAcrobacia} />
+              <input type="text" className="numero-pericia numero-acrobacia" maxLength={2} ref={inputNumeroPericiaAcrobacia} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-arcanismo" ref={inputPericiaArcanismo} />
+              <input type="text" className="numero-pericia numero-arcanismo" maxLength={2} ref={inputNumeroPericiaArcanismo} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-atletismo" ref={inputPericiaAtletismo} />
+              <input type="text" className="numero-pericia numero-atletismo" maxLength={2} ref={inputNumeroPericiaAtletismo} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-atuacao" ref={inputPericiaAtuacao} />
+              <input type="text" className="numero-pericia numero-atuacao" maxLength={2} ref={inputNumeroPericiaAtuacao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-enganacao" ref={inputPericiaEnganacao} />
+              <input type="text" className="numero-pericia numero-enganacao" maxLength={2} ref={inputNumeroPericiaEnganacao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-furtividade" ref={inputPericiaFurtividade} />
+              <input type="text" className="numero-pericia numero-furtividade" maxLength={2} ref={inputNumeroPericiaFurtividade} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-historia" ref={inputPericiaHistoria} />
+              <input type="text" className="numero-pericia numero-historia" maxLength={2} ref={inputNumeroPericiaHistoria} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-intimidacao" ref={inputPericiaIntimidacao} />
+              <input type="text" className="numero-pericia numero-intimidacao" maxLength={2} ref={inputNumeroPericiaIntimidacao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-intuicao" ref={inputPericiaIntuicao} />
+              <input type="text" className="numero-pericia numero-intuicao" maxLength={2} ref={inputNumeroPericiaIntuicao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-investigacao" ref={inputPericiaInvestigacao} />
+              <input type="text" className="numero-pericia numero-investigacao" maxLength={2} ref={inputNumeroPericiaInvestigacao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-lidar-com-animais" ref={inputPericiaLidarComAnimais} />
+              <input type="text" className="numero-pericia numero-lidar-com-animais" maxLength={2} ref={inputNumeroPericiaLidarComAnimais} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-medicina" ref={inputPericiaMedicina} />
+              <input type="text" className="numero-pericia numero-medicina" maxLength={2} ref={inputNumeroPericiaMedicina} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-natureza" ref={inputPericiaNatureza} />
+              <input type="text" className="numero-pericia numero-natureza" maxLength={2} ref={inputNumeroPericiaNatureza} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-percepcao" ref={inputPericiaPercepcao} />
+              <input type="text" className="numero-pericia numero-percepcao" maxLength={2} ref={inputNumeroPericiaPercepcao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-persuasao" ref={inputPericiaPersuasao} />
+              <input type="text" className="numero-pericia numero-persuasao" maxLength={2} ref={inputNumeroPericiaPersuasao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-prestidigitacao" ref={inputPericiaPrestidigitacao} />
+              <input type="text" className="numero-pericia numero-prestidigitacao" maxLength={2} ref={inputNumeroPericiaPrestidigitacao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-religiao" ref={inputPericiaReligiao} />
+              <input type="text" className="numero-pericia numero-religiao" maxLength={2} ref={inputNumeroPericiaReligiao} />
+            </div>
+            <div className="linha-pericia">
+              <input type="checkbox" className="bolinha bolinha-sobrevivencia" ref={inputPericiaSobrevivencia} />
+              <input type="text" className="numero-pericia numero-sobrevivencia" maxLength={2} ref={inputNumeroPericiaSobrevivencia} />
+            </div>
+          </div>
         </div>
         <button className="botaoSalvar">Salvar</button>
       </div>
